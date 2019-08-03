75066cf8b5dd701e76a2ade7ddce12445e60b2c9
Bogdan <bogdan7morozyuk@gmail.com>
2019-08-03 18:27:39 +0300

    Define routes

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import express, { Application } from 'express';
 import config from 'config';
 import mongoose from 'mongoose';
 
+import ItemRouter from './routes/item.router';
+
 const app: Application = express();
 
 const port: number = config.get('port') || 5000;
@@ -21,6 +23,8 @@ mongoose
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use('/api/items', ItemRouter);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
