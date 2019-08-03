36e354d8fad4a633e5f9877bb975b418efa4bc74
Bogdan <bogdan7morozyuk@gmail.com>
2019-08-03 18:55:43 +0300

    Import bootstrap styles

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,6 +4,7 @@ import * as serviceWorker from 'serviceWorker';
 
 import App from 'App';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
 import 'index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
