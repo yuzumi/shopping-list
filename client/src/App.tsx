abda7fe0249176e267c151ce8c73f9110064ddb1
Bogdan <bogdan7morozyuk@gmail.com>
2019-08-03 20:17:43 +0300

    Import ShoppingList component; Add test data and operations for working with this data

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,46 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useState } from 'react';
 import AppNavbar from 'components/layout/AppNavbar';
+import ShoppingList from 'components/list/ShoppingList';
+import { ItemList, Item } from 'Models';
+import { v4 } from 'uuid';
 
-const App: FunctionComponent = () => (
-  <div className="app">
-    <AppNavbar />
-  </div>
-);
+// Just for example
+const initialItems: ItemList = [
+  {
+    _id: v4(),
+    name: 'Milk',
+    date: new Date()
+  },
+  {
+    _id: v4(),
+    name: 'Eggs',
+    date: new Date()
+  }
+];
+
+const App: FunctionComponent = () => {
+  const [items, setItems] = useState<ItemList>(initialItems);
+
+  const deleteItem = (itemId: string | number): void => {
+    const updatedItems: ItemList = items.filter(
+      (item: Item): boolean => {
+        return item._id !== itemId;
+      }
+    );
+
+    setItems(updatedItems);
+  };
+
+  const addItem = (item: Item): void => {
+    setItems([item, ...items]);
+  };
+
+  return (
+    <div className="app">
+      <AppNavbar />
+      <ShoppingList items={items} deleteItem={deleteItem} />
+    </div>
+  );
+};
 
 export default App;
