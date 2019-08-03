7958eec64708d5271ac1095a68033e92b83a2090
Bogdan <bogdan7morozyuk@gmail.com>
2019-08-03 22:48:23 +0300

    Connect the app to the store

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { FunctionComponent } from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from 'serviceWorker';
+
+import { Provider } from 'react-redux';
+import store from 're-ducks/store';
 
 import App from 'App';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Root: FunctionComponent = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+ReactDOM.render(<Root />, document.getElementById('root'));
 
 serviceWorker.unregister();
