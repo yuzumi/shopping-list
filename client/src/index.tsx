c766bd95d9b3894541339efbaf514e4406fa7f05
Bogdan <bogdan7morozyuk@gmail.com>
2019-08-03 18:36:51 +0300

    Set baseUrl

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from 'serviceWorker';
 
-import App from './App';
+import App from 'App';
 
-import './index.css';
+import 'index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
