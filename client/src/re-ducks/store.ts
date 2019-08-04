4bc7c34209f84d972376baa70b4bc330c15be139
Bogdan <bogdan7morozyuk@gmail.com>
2019-08-04 11:10:17 +0300

    Apply middleware

--- client/src/re-ducks/store.ts
+++ client/src/re-ducks/store.ts
@@ -1,6 +1,7 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import rootReducer from 're-ducks/reducer';
+import middleware from 're-ducks/middleware';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(...middleware));
 
 export default store;
