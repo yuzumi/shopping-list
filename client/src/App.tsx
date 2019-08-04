4db3ad58004977f89be267cd97f8f2768cd150fa
Bogdan <bogdan7morozyuk@gmail.com>
2019-08-04 21:52:23 +0300

    Apply Template component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,20 @@
 import React, { FunctionComponent } from 'react';
-import AppNavbar from 'components/layout/AppNavbar';
 import ItemCreator from 'components/ItemCreator';
 import ShoppingList from 'components/list/ShoppingList';
 import Section from 'components/layout/Section';
+import Template from 'components/layout/Template';
 
 const App: FunctionComponent = () => {
   return (
     <div className="app">
-      <AppNavbar />
-      <Section>
-        <ItemCreator />
-      </Section>
-      <Section>
-        <ShoppingList />
-      </Section>
+      <Template>
+        <Section>
+          <ItemCreator />
+        </Section>
+        <Section>
+          <ShoppingList />
+        </Section>
+      </Template>
     </div>
   );
 };
