8ef71c4cac3c1ac4ecae4f3bfd40b7fbeda39ce0
Bogdan <bogdan7morozyuk@gmail.com>
2019-08-04 12:48:54 +0300

    Combine item reducer

--- client/src/re-ducks/reducer.ts
+++ client/src/re-ducks/reducer.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
+import itemReducer from 're-ducks/modules/item';
 
 const rootReducer = combineReducers({
-  form: formReducer
+  form: formReducer,
+  item: itemReducer
 });
 
 export default rootReducer;
