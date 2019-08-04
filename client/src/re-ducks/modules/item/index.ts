cbe8c3d6a72e90720c3cec2151b89b702c3500ff
Bogdan <bogdan7morozyuk@gmail.com>
2019-08-04 12:48:36 +0300

    Create item module: root

---
+++ client/src/re-ducks/modules/item/index.ts
@@ -0,0 +1,7 @@
+import reducer from 're-ducks/modules/item/reducer';
+
+export { default as types } from 're-ducks/modules/item/types';
+export { default as actions } from 're-ducks/modules/item/actions';
+export { default as selectors } from 're-ducks/modules/item/selectors';
+
+export default reducer;
