9f7a393bda59224431b5cd6621155c2b0e3a94d7
Bogdan <bogdan7morozyuk@gmail.com>
2019-08-04 21:54:06 +0300

    Create AppBrand component

---
+++ client/src/components/layout/AppBrand.tsx
@@ -0,0 +1,8 @@
+import React, { FunctionComponent } from 'react';
+import { NavbarBrand } from 'reactstrap';
+
+const AppBrand: FunctionComponent = () => (
+  <NavbarBrand href="/">Shopping List</NavbarBrand>
+);
+
+export default AppBrand;
