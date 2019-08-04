175b7ab27eb10fc7df38493984b173c800683455
Bogdan <bogdan7morozyuk@gmail.com>
2019-08-04 13:06:16 +0300

    Add type: ITEMS_LOADING

--- client/src/re-ducks/modules/item/types.ts
+++ client/src/re-ducks/modules/item/types.ts
@@ -1,9 +1,11 @@
 const ADD_ITEM = 'shopping-list/item/ADD_ITEM';
 const GET_ITEMS = 'shopping-list/item/GET_ITEMS';
 const DELETE_ITEM = 'shopping-list/item/DELETE_ITEM';
+const ITEMS_LOADING = 'shopping-list/item/ITEMS_LOADING';
 
 export default {
   ADD_ITEM,
   GET_ITEMS,
-  DELETE_ITEM
+  DELETE_ITEM,
+  ITEMS_LOADING
 };
