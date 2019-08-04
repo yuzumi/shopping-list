81b17afa0f66efdfab65c23ca58afb7eaaf816b7
Bogdan <bogdan7morozyuk@gmail.com>
2019-08-04 12:47:52 +0300

    Create item module: types

---
+++ client/src/re-ducks/modules/item/types.ts
@@ -0,0 +1,9 @@
+const ADD_ITEM = 'shopping-list/item/ADD_ITEM';
+const GET_ITEMS = 'shopping-list/item/GET_ITEMS';
+const DELETE_ITEM = 'shopping-list/item/DELETE_ITEM';
+
+export default {
+  ADD_ITEM,
+  GET_ITEMS,
+  DELETE_ITEM
+};
