7b2bd342e5b76667a3e7c54cc7d0f2e7d55a0986
Bogdan <bogdan7morozyuk@gmail.com>
2019-08-04 12:25:33 +0300

    Create ItemForm component

---
+++ client/src/components/form/ItemForm.tsx
@@ -0,0 +1,59 @@
+import React, { FunctionComponent, FormEvent, MouseEvent } from 'react';
+import { Form, FormGroup, Label, Input, Button } from 'reactstrap';
+
+export interface ItemFormProps {
+  value: string;
+  onChange: (event: FormEvent<HTMLInputElement>) => void;
+  onSubmit: (event: FormEvent<HTMLFormElement>) => void;
+  onReset?: (event: MouseEvent<HTMLButtonElement>) => void;
+  onCancel?: (event: MouseEvent<HTMLButtonElement>) => void;
+}
+
+const ItemForm: FunctionComponent<ItemFormProps> = ({
+  value,
+  onChange,
+  onSubmit,
+  onReset,
+  onCancel
+}) => (
+  <Form onSubmit={onSubmit}>
+    <FormGroup>
+      <Label htmlFor="name">Name</Label>
+      <Input
+        type="text"
+        name="name"
+        id="name"
+        placeholder="Enter the item name"
+        value={value}
+        onChange={onChange}
+      />
+    </FormGroup>
+    <Button color="light" size="sm" type="submit">
+      Add
+    </Button>
+    {onReset && (
+      <Button
+        className="ml-2"
+        color="warning"
+        size="sm"
+        type="button"
+        onClick={onReset}
+      >
+        Reset
+      </Button>
+    )}
+    {onCancel && (
+      <Button
+        className="ml-2"
+        color="danger"
+        size="sm"
+        type="button"
+        onClick={onCancel}
+      >
+        Cancel
+      </Button>
+    )}
+  </Form>
+);
+
+export default ItemForm;
