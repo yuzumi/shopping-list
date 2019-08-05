bcbea1473961d51cc8228c8b2c3cc8cf8372f04c
Bogdan <bogdan7morozyuk@gmail.com>
2019-08-05 22:55:43 +0300

    Apply auth middleware

--- server/routes/item.router.ts
+++ server/routes/item.router.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import ItemController from '../controllers/item.controller';
+import auth from '../middleware/auth.middleware';
 
 const ItemRouter: Router = Router();
 
-ItemRouter.get('/', ItemController.get)
-  .post('/', ItemController.create)
-  .delete('/:id', ItemController.delete);
+ItemRouter
+  .get('/', ItemController.get)
+  .post('/', auth, ItemController.create)
+  .delete('/:id', auth, ItemController.delete);
 
 export default ItemRouter;
